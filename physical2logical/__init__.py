2cc566e9d5cb3b001378db563d48b8b411135971
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-01-18 22:03:38 +0100

    fix: release ci

--- physical2logical/__init__.py
+++ physical2logical/__init__.py
@@ -1,19 +1,5 @@
 # Copyright 2023 Dragan Filipovic
-
 # Based on https://gist.github.com/nyurik/d438cb56a9059a0660ce4176ef94576f
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 
 name = "physical2logical"
 VERSION = "1.2.0"
